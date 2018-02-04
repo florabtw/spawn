65227006311eb7942f42d2c84c15f3fc269bef24
Nick Pierson <nick@nickpierson.name>
2018-02-04 02:58:30 +0000

    Add tmux to ubuntu-dev

--- ubuntu-dev/install.sh
+++ ubuntu-dev/install.sh
@@ -48,3 +48,18 @@ nvm install --lts
 
 sudo service mongod start
 systemctl enable mongod.service
+
+## Tmux
+
+sudo apt-get remove tmux -y
+sudo apt-get install build-essential libevent-dev ncurses-dev -y
+
+pushd /tmp/
+
+wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
+tar -xzf tmux-2.6.tar.gz
+cd tmux-2.6
+./configure && make
+sudo make install
+
+popd
