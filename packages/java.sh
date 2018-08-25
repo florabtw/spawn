c6b74a90b3d46fa09e0ff2a433ec74b4d6f0cc05
Nick Pierson <nick@nickpierson.name>
2018-08-25 02:07:55 +0000

    fix runelite install

--- packages/java.sh
+++ packages/java.sh
@@ -2,11 +2,10 @@
 
 sudo apt-get install software-properties-common -y
 
-sudo add-apt-repository ppa:linuxuprising/java -y
+sudo add-apt-repository ppa:linuxuprising/java
 
 sudo apt-get update
 
 sudo apt-get install        \
   oracle-java10-installer   \
-  oracle-java10-set-default \
-  -y
+  oracle-java10-set-default
