fd78d0d646a4cb8fa7e4a2c469fc97956a32319f
Nick Pierson <nick@nickpierson.name>
2018-08-25 01:27:57 +0000

    add runelite, java to packages

---
+++ packages/java.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo apt-get install software-properties-common -y
+
+sudo add-apt-repository ppa:linuxuprising/java
+
+sudo apt-get update
+
+sudo apt-get install        \
+  oracle-java10-installer   \
+  oracle-java10-set-default
---
+++ packages/runelite.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+./packages/java.sh
+
+sudo apt-get install curl
+
+curl -L -o /tmp/runelite.jar "https://github.com/runelite/launcher/releases/download/1.6.0/RuneLite.jar"
+curl -L -o /tmp/runelite.png "https://runelite.net/img/logo.png"
+
+sudo mkdir /opt/runelite
+sudo mv /tmp/runelite.jar /opt/runelite/
+sudo mv /tmp/runelite.png /opt/runelite/
+
+sudo bash -c 'cat <<EOF > /opt/runelite/run.sh
+java -jar /opt/runelite/runelite.jar --mode=OFF
+EOF'
+
+sudo chmod +x /opt/runelite/run.sh
+
+sudo bash -c 'cat << EOF > /usr/share/applications/runelite.desktop
+[Desktop Entry]
+Version=1.0
+Name=RuneLite
+Comment=Play Runescape!
+Icon=/opt/runelite/runelite.png
+Exec=/opt/runelite/runelite.sh
+Terminal=false
+Type=Application
+Categories=Network
+EOF'
