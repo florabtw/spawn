64c008271d185c7b4a60a9c0442a021364d86dda
Nick Pierson <nick@nickpierson.name>
2018-09-03 08:03:45 +0000

    add gnupg to java install package

--- packages/java.sh
+++ packages/java.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-sudo apt-get install software-properties-common -y
+sudo apt-get install         \
+  gnupg                      \
+  software-properties-common \
+  -y
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
 sudo add-apt-repository ppa:linuxuprising/java
