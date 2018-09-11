a49031180e1664243ade749ef5bcaffcfc125837
Nick Pierson <nick@nickpierson.name>
2018-09-11 02:39:50 +0000

    remove prompts from java install script

--- packages/java.sh
+++ packages/java.sh
@@ -6,10 +6,11 @@ sudo apt-get install         \
   -y
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
-sudo add-apt-repository ppa:linuxuprising/java
+sudo add-apt-repository ppa:linuxuprising/java -y
 
 sudo apt-get update
 
 sudo apt-get install        \
   oracle-java10-installer   \
-  oracle-java10-set-default
+  oracle-java10-set-default \
+  -y
