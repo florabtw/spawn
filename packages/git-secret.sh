25450c4ce9d27392d84ba83ccaad3195fb078a33
Nick Pierson <me@nick.exposed>
2018-10-25 00:00:09 +0000

    add git-secret package

---
+++ packages/git-secret.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
+
+wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
+
+sudo apt-get update && sudo apt-get install git-secret
