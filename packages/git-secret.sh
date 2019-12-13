967b3b10e03eaf55f4126395b249bd530abe760c
Nick Pierson <me@nick.exposed>
2019-12-13 23:25:33 +0000

    add wget to git-secret package

--- packages/git-secret.sh
+++ packages/git-secret.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install wget
+
 echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
 
 wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
