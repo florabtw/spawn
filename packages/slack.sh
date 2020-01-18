6c8a61ef0fbc69e0f7a1080fcd4b95875e63d2c9
Nick Pierson <me@nick.exposed>
2020-01-18 00:00:53 -0600

    update slack version

--- packages/slack.sh
+++ packages/slack.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install curl
 
-curl -L -o /tmp/slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb"
+curl -L -o /tmp/slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.1.1-amd64.deb"
 sudo dpkg -i /tmp/slack.deb
 
 sudo apt-get --fix-broken install -y
