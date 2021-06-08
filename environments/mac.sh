a33ce0679f38f0f00f888e7057ac05358f2c0df7
Nick Pierson <me@nick.exposed>
2021-06-08 13:19:27 -0700

    update mac install scripts

--- environments/mac.sh
+++ environments/mac.sh
@@ -2,9 +2,9 @@
 
 ./packages/mac/brew.sh
 ./packages/mac/dotfiles.sh
-./packages/mac/tmux.sh
 
 ./packages/node.sh
 
 brew install \
-  tmate
+  gpg        \
+  tmux
--- packages/mac/node.sh
+++ packages/mac/node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.38.0/install.sh | bash
 
 source ~/.nvm/nvm.sh
 
